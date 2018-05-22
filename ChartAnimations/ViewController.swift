5cacf6fcff6d84edbbda91ba1742df64a03c8d69
mrformidable <MichaelAi@michaels-macbook-pro-3.local>
2018-05-22 17:26:56 -0400

    Initial Commit

---
+++ ChartAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChartAnimations
+//
+//  Created by Michael A on 2018-05-22.
+//  Copyright © 2018 AI Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
